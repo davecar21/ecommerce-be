74b47b412b3abbc050be5d745ed7e306264e4a4b
davecar <davecargrave@gmail.com>
2019-02-19 17:40:01 +0800

    added mongoose

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ git push --set-upstream origin master
 
 
 #Heroku cli
+heroku login
 heroku logs --tail --app ecommerce-be
 heroku restart --app ecommerce-be
--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 const express = require('express');
 const app = express();
+
 const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // for parsing application/x-www-form-urlencoded
+
+
+
+mongoose.connect('mongodb://tester1:tester1@ds057862.mlab.com:57862/ecommerce-db', {
+        useNewUrlParser: true
+    })
+    .then(() => {
+        console.log('db connection SUCCESS')
+    })
+    .catch((error) => {
+        console.log('db connection FAILED')
+    });
+
+
+var schema = new mongoose.Schema({
+    name: 'string',
+    size: 'string'
+});
+var Tank = mongoose.model('Tank', schema);
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/', (req, res) => {
+    var data = new Tank(req.body); 
+    data.save(function (err) {
+        if (err) {
+            return handleError(err)
+        };
+        res.send('SAVED!')
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
